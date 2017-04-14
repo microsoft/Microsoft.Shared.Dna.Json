843d60b258c0c32dfdcc540b86b27c00e9be04ce
Rich Daniel <rdaniel@microsoft.com>
2017-04-14 11:41:02 -0700

    Simplify build script instructions. (#5)

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
 @ECHO OFF
+SETLOCAL
 PUSHD "%~dp0"
 IF NOT EXIST .tools CALL init.cmd
+IF NOT DEFINED VS140COMNTOOLS GOTO NEEDVS
+IF NOT DEFINED DevEnvDir CALL "%VS140COMNTOOLS%\VsDevCmd.bat"
 .tools\nuget restore
 msbuild build.proj /verbosity:n /clp:ShowCommandLine /m:%NUMBER_OF_PROCESSORS% /nr:false /fl /flp:LogFile=MSBuild.log;Verbosity=diag;ShowTimestamp
 FOR %%I IN (Debug Release) DO (
@@ -11,4 +14,10 @@ FOR %%I IN (Debug Release) DO (
     vstest.console "%%~J"
   )
 )
+GOTO END
+:NEEDVS
+ECHO Install Visual Studio 2015 to build.
+GOTO END
+:END
 POPD
+ENDLOCAL
