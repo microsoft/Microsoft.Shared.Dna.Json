7eaf669cc39b952dd3d498369e9911640e14dbaa
Rich Daniel <rdaniel@microsoft.com>
2021-08-27 10:28:58 -0700

    Fix multi-feed issues.

--- build.cmd
+++ build.cmd
@@ -2,21 +2,20 @@
 SETLOCAL
 PUSHD "%~dp0"
 IF NOT EXIST .tools CALL init.cmd
-IF NOT DEFINED VS140COMNTOOLS GOTO NEEDVS
-IF NOT DEFINED DevEnvDir CALL "%VS140COMNTOOLS%\VsDevCmd.bat"
+msbuild -help >NUL 2>&1
+IF ERRORLEVEL 9009 GOTO NEEDVS
+vstest.console -? >NUL 2>&1
+IF ERRORLEVEL 9009 GOTO NEEDVS
 .tools\nuget restore
 msbuild build.proj /verbosity:n /clp:ShowCommandLine /m:%NUMBER_OF_PROCESSORS% /nr:false /fl /flp:LogFile=MSBuild.log;Verbosity=diag;ShowTimestamp
 FOR %%I IN (Debug Release) DO (
   FOR /R %%J IN (bin\%%~I\*.Test.dll) DO (
     vstest.console "%%~J"
   )
-  FOR /R %%J IN (bin\%%~I\*.Profile.exe) DO (
-    vstest.console "%%~J"
-  )
 )
 GOTO END
 :NEEDVS
-ECHO Install Visual Studio 2015 to build.
+ECHO Run %~nx0 from a Visual Studio developer command prompt.
 GOTO END
 :END
 POPD
